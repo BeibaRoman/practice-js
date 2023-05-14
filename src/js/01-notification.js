5d3d25cc0c132a4e5858f9fb012c7a342f88e325
BeibaRoman <106346781+BeibaRoman@users.noreply.github.com>
2023-05-14 15:06:45 +0300

    add first task

---
+++ src/js/01-notification.js
@@ -0,0 +1,30 @@
+import '../css/common.css';
+
+let timerId = null;
+const NOTIFICATION_DELAY = 3000;
+
+refs = {
+  notificationEl: document.querySelector('.js-alert'),
+};
+
+refs.notificationEl.addEventListener('click', onNotificationClick);
+
+showNotification();
+
+function onNotificationClick() {
+  console.log('Прибираємо нагадування та видаляємо сеттаймаут');
+  onCloseNotification();
+  clearTimeout(timerId);
+}
+
+function showNotification() {
+  refs.notificationEl.classList.add('is-visible');
+  timerId = setTimeout(() => {
+    console.log('Закриваємо нагадування черз 3 секунди!');
+    onCloseNotification();
+  }, NOTIFICATION_DELAY);
+}
+
+function onCloseNotification() {
+  refs.notificationEl.classList.remove('is-visible');
+}
