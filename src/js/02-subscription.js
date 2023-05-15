dfdddf4acc36205c440b7ea845ff1628ecbeb7ea
BeibaRoman <106346781+BeibaRoman@users.noreply.github.com>
2023-05-15 18:57:09 +0300

    add modal and timer

--- src/js/02-subscription.js
+++ src/js/02-subscription.js
@@ -1,1 +1,63 @@
-console.log('hello');
+import '../css/modal.css';
+
+const refs = {
+  backdrop: document.querySelector('.js-backdrop'),
+  closeModalBtn: document.querySelector('[data-action="close-modal"]'),
+  subscribeModalBtn: document.querySelector('[data-action="subscribe-modal"]'),
+};
+
+const PROMP_DELAY = 3000;
+const MAX_PROMP_ATTEMPTS = 3;
+let promtCounter = 0;
+let hasSubscribed = false;
+
+refs.closeModalBtn.addEventListener('click', onCloseModal);
+refs.subscribeModalBtn.addEventListener('click', onSubscriveModal);
+refs.backdrop.addEventListener('click', onBackdropClick);
+
+openModalWithDelay();
+
+function openModalWithDelay() {
+  if (promtCounter === MAX_PROMP_ATTEMPTS || hasSubscribed) {
+    console.log('Останнє логування!');
+    return;
+  }
+
+  setTimeout(() => {
+    onOpenModal();
+    promtCounter += 1;
+  }, PROMP_DELAY);
+}
+
+function onOpenModal() {
+  window.addEventListener('keydown', onEscKeyPress);
+  document.body.classList.add('show-modal');
+  console.log('Сетап в роботі!');
+}
+
+function onCloseModal() {
+  window.removeEventListener('keydown', onEscKeyPress);
+  document.body.classList.remove('show-modal');
+  openModalWithDelay();
+}
+
+function onSubscriveModal() {
+  console.log('Вітаю, Ви підписалися на нашу розсилку!');
+  hasSubscribed = true;
+  onCloseModal();
+}
+
+function onBackdropClick(e) {
+  if (e.currentTarget === e.target) {
+    onCloseModal();
+  }
+}
+
+function onEscKeyPress(e) {
+  const ESC_KEY = 'Escape';
+  const isEscKey = e.code === ESC_KEY;
+  if (isEscKey) {
+    console.log('Закриваємо модалку по натиску на ESC');
+    onCloseModal();
+  }
+}
--- src/js/03-timer.js
+++ src/js/03-timer.js
@@ -1,1 +1,68 @@
-console.log('hello');
+import '../css/common.css';
+
+const refs = {
+  startBtn: document.querySelector('button[data-action-start]'),
+  stopBtn: document.querySelector('button[data-action-stop]'),
+  clockface: document.querySelector('.js-clockface'),
+};
+
+class Timer {
+  constructor({ onTick }) {
+    this.intervalID = null;
+    this.isActive = false;
+    this.onTick = onTick;
+    this.init();
+  }
+
+  init() {
+    const timeComponents = this.getTimeComponents(0);
+    this.onTick(timeComponents);
+  }
+
+  start() {
+    if (this.isActive) {
+      return;
+    }
+    const startTime = Date.now();
+    this.isActive = true;
+
+    this.intervalID = setInterval(() => {
+      const currentTime = Date.now();
+      const deltaTime = currentTime - startTime;
+      const timeComponents = this.getTimeComponents(deltaTime);
+      this.onTick(timeComponents);
+    }, 1000);
+  }
+
+  stop() {
+    clearInterval(this.intervalID);
+    this.isActive = false;
+    const timeComponents = this.getTimeComponents(0);
+    this.onTick(timeComponents);
+  }
+
+  getTimeComponents(time) {
+    const hours = this.pad(
+      Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+    );
+    const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+    const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+
+    return { hours, mins, secs };
+  }
+
+  pad(value) {
+    return String(value).padStart(2, '0');
+  }
+}
+
+const timer = new Timer({
+  onTick: updateClockface,
+});
+
+refs.startBtn.addEventListener('click', timer.start.bind(timer));
+refs.stopBtn.addEventListener('click', timer.stop.bind(timer));
+
+function updateClockface({ hours, mins, secs }) {
+  refs.clockface.textContent = `${hours}:${mins}:${secs}`;
+}
